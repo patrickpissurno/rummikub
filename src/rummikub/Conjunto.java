a5b2c7fe42514bef7cbcbc91f5dac535d8e787c2
Luciano Hanna <lucianohanna@id.uff.br>
2019-05-19 11:51:37 -0300

    Implementada Issue #7

--- src/rummikub/Conjunto.java
+++ src/rummikub/Conjunto.java
@@ -1,7 +1,8 @@
 package rummikub;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
-import java.util.List;
 
 public class Conjunto {
     private LinkedList<Pedra> pedras;
@@ -34,5 +35,35 @@ public class Conjunto {
         }
 
         return true;
+    }
+
+    public boolean isSequencia(){
+        if (pedras.size() < 3)
+            return false;
+
+        String corDoGrupo = pedras.getFirst().getCor();
+        ArrayList<Integer> numeros = new ArrayList<>();
+        int nCoringas = 0;
+
+        for(int i = 0; i < pedras.size(); i++) {
+            // verifica se tem alguma pedra de numero diferente
+            if (!pedras.get(i).getTipo().equals(Pedra.TIPO_CORINGA) && !pedras.get(i).getCor().equals(corDoGrupo))
+                return false;
+
+            if (pedras.get(i).getTipo().equals(Pedra.TIPO_CORINGA))
+                nCoringas++;
+            else
+                numeros.add(Integer.parseInt(pedras.get(i).getTipo()));
+        }
+
+        Collections.sort(numeros);
+
+        // ultimo - primeiro <= tamanho - 1 + nCoringas
+        // < para os casos em que o coringa está na borda
+        // == para os casos em que o coringa está no meio ou não existe
+        if (numeros.get(numeros.size() - 1) - numeros.get(0) <= numeros.size() - 1 + nCoringas)
+            return true;
+
+        return false;
     }
 }
--- src/test/ConjuntoTest.java
+++ src/test/ConjuntoTest.java
@@ -1,7 +1,5 @@
 package test;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import rummikub.Conjunto;
 import rummikub.Pedra;
@@ -112,6 +110,77 @@ class ConjuntoTest {
         Conjunto conjunto = new Conjunto(pedras);
 
         assertFalse(conjunto.isGrupo());
+    }
+
+    @Test
+    void sequenciaDe3Pedras() {
+        LinkedList<Pedra> pedras = new LinkedList<>();
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_1, Pedra.COR_AZUL));
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_2, Pedra.COR_AZUL));
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_3, Pedra.COR_AZUL));
+
+        Conjunto conjunto = new Conjunto(pedras);
+
+        assertTrue(conjunto.isSequencia());
+    }
+
+    @Test
+    void sequenciaDe3PedrasDesordenado() {
+        LinkedList<Pedra> pedras = new LinkedList<>();
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_1, Pedra.COR_AZUL));
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_3, Pedra.COR_AZUL));
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_2, Pedra.COR_AZUL));
+
+        Conjunto conjunto = new Conjunto(pedras);
+
+        assertTrue(conjunto.isSequencia());
+    }
+
+    @Test
+    void sequenciaDe3PedrasComCoringaNoMeio() {
+        LinkedList<Pedra> pedras = new LinkedList<>();
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_1, Pedra.COR_AZUL));
+        pedras.add(new Pedra(Pedra.TIPO_CORINGA, Pedra.COR_AZUL));
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_3, Pedra.COR_AZUL));
+
+        Conjunto conjunto = new Conjunto(pedras);
+
+        assertTrue(conjunto.isSequencia());
+    }
+
+    @Test
+    void sequenciaDe3PedrasComCoringaNaBorda() {
+        LinkedList<Pedra> pedras = new LinkedList<>();
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_1, Pedra.COR_AZUL));
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_2, Pedra.COR_AZUL));
+        pedras.add(new Pedra(Pedra.TIPO_CORINGA, Pedra.COR_AZUL));
+
+        Conjunto conjunto = new Conjunto(pedras);
+
+        assertTrue(conjunto.isSequencia());
+    }
+
+
+    @Test
+    void sequenciaDe2Pedras() {
+        LinkedList<Pedra> pedras = new LinkedList<>();
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_1, Pedra.COR_AZUL));
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_2, Pedra.COR_AZUL));
+
+        Conjunto conjunto = new Conjunto(pedras);
+
+        assertFalse(conjunto.isSequencia());
+    }
+
+    @Test
+    void sequenciaDe2PedrasComCoringa() {
+        LinkedList<Pedra> pedras = new LinkedList<>();
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_1, Pedra.COR_AZUL));
+        pedras.add(new Pedra(Pedra.TIPO_CORINGA, Pedra.COR_AZUL));
+
+        Conjunto conjunto = new Conjunto(pedras);
+
+        assertFalse(conjunto.isSequencia());
     }
 
 }
