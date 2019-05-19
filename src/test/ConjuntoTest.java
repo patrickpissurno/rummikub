7480f974c5f20b554263c5acdb55d1a71ad6007a
Luciano Hanna <lucianohanna@id.uff.br>
2019-05-19 11:16:04 -0300

    Adicionado casos de teste a Issue #6

--- src/test/ConjuntoTest.java
+++ src/test/ConjuntoTest.java
@@ -9,8 +9,7 @@ import rummikub.Pedra;
 
 import java.util.LinkedList;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ConjuntoTest {
 
@@ -63,6 +62,56 @@ class ConjuntoTest {
         Conjunto conjunto = new Conjunto(pedras);
 
         assertTrue(conjunto.isGrupo());
+    }
+
+    @Test
+    void grupoDe4CoresRepetidas() {
+        LinkedList<Pedra> pedras = new LinkedList<>();
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_1, Pedra.COR_AZUL));
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_1, Pedra.COR_ROSA));
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_1, Pedra.COR_VERDE));
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_1, Pedra.COR_VERDE));
+
+        Conjunto conjunto = new Conjunto(pedras);
+
+        assertFalse(conjunto.isGrupo());
+    }
+
+    @Test
+    void grupoDe3CoresRepetidas() {
+        LinkedList<Pedra> pedras = new LinkedList<>();
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_1, Pedra.COR_AZUL));
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_1, Pedra.COR_ROSA));
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_1, Pedra.COR_AZUL));
+
+        Conjunto conjunto = new Conjunto(pedras);
+
+        assertFalse(conjunto.isGrupo());
+    }
+
+    @Test
+    void grupoDe4CoresRepetidasComCoringa() {
+        LinkedList<Pedra> pedras = new LinkedList<>();
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_1, Pedra.COR_AZUL));
+        pedras.add(new Pedra(Pedra.TIPO_CORINGA, Pedra.COR_ROSA));
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_1, Pedra.COR_ROSA));
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_1, Pedra.COR_VERMELHO));
+
+        Conjunto conjunto = new Conjunto(pedras);
+
+        assertFalse(conjunto.isGrupo());
+    }
+
+    @Test
+    void grupoDe3CoresRepetidaComCoringa() {
+        LinkedList<Pedra> pedras = new LinkedList<>();
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_1, Pedra.COR_AZUL));
+        pedras.add(new Pedra(Pedra.TIPO_CORINGA, Pedra.COR_AZUL));
+        pedras.add(new Pedra(Pedra.TIPO_NUMERO_1, Pedra.COR_VERDE));
+
+        Conjunto conjunto = new Conjunto(pedras);
+
+        assertFalse(conjunto.isGrupo());
     }
 
 }
